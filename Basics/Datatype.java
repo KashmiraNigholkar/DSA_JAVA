6a3fd5ffba388c81068f1153f92974b8e9706bf1
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-04-04 06:56:51 +0530

    Add files via upload

---
+++ Basics/Datatype.java
@@ -0,0 +1,18 @@
+public class Datatype{
+    public static void main (String args[]){
+        char r='c';
+        byte a=9;
+        boolean var=false;
+        double Price=10.8;
+        int number=25;
+        short n=240;
+
+        System.out.println(r);
+        System.out.println(a);
+        System.out.println(var);
+        System.out.println(number);
+        System.out.println(n);
+
+    }
+
+}
