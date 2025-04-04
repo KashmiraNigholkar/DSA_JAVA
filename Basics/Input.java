65aaf153e8e22483f01014f42667948e4972c977
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-04-04 22:14:37 +0530

    Add files via upload

---
+++ Basics/Input.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Input{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        // String input=sc.next();
+        // System.out.print("My name is kashmira",input);
+
+
+        // int a=sc.nextInt();
+        // System.out.println(a)
+        // ;
+
+        // byte s=sc.nextByte();
+        // System.out.println(s);
+
+        // float a=sc.nextFloat();
+        // System.out.println(a);
+
+        // double s=sc.nextDouble();
+        // System.out.println(s);
+
+        // boolean b=sc.nextBoolean();
+        // System.out.println(b);
+
+        // short s=sc.nextShort();
+        // System.out.println(s);
+
+
+        // long l=sc.nextLong();
+        // System.out.println(l);
+
+
+
+        // String s=sc.nextLine();
+        // System.out.println(s);
+
+
+
+    }
+}
