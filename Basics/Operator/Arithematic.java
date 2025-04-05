539fcebea3e81f1328bcc23088f36cd8ee19d973
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-04-05 22:50:32 +0530

    Add files via upload

---
+++ Basics/Operator/Arithematic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arithematic{
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=10;
+        int b=34;
+        System.out.println("add="+(a+b));
+        System.out.println("sub="+(a-b));
+        System.out.println("mul="+(a*b));
+        System.out.println("div="+(a/b));
+
+    }
+}
