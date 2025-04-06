3a3cd7c5ede00160e8d289974660882d284db488
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-04-06 09:35:28 +0530

    Add files via upload

---
+++ Operator/Assignment.java
@@ -0,0 +1,27 @@
+public class Assignment{
+    public static void main(String args[]){
+        int a=10;
+        int b=7;
+        a+=10;
+        System.out.println(a);
+        b+=19;
+        System.out.println(b);
+
+        a-=19;
+        System.out.println(a);
+        b-=4;
+        System.out.println(b);
+        a*=3;
+        System.out.println(a);
+        b*=5;
+        System.out.println(b);
+
+        a/=4;
+        System.out.println(a);
+        b/=6;
+        System.out.println(b);
+
+
+
+    }
+}
---
+++ Operator/Logical.java
@@ -0,0 +1,9 @@
+public class Logical{
+    public static void main(String args[]){
+        System.out.println((3>2)&&(5<0));
+        System.out.println((3<2)||(5<0));
+        System.out.println(!(0>5));
+
+
+    }
+}
