1e772fb22fd24541dc7f8f9ef77cb6c269e53ba4
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-04-08 22:01:11 +0530

    Add files via upload

---
+++ Loops/PrintNnum.java
@@ -0,0 +1,15 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class PrintNnum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int range=sc.nextInt();
+        int counter=1;
+        while(counter<=range){
+            System.out.println(counter +" ");
+        }
+    }s
+    
+}
---
+++ Loops/PrintNumW.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class PrintNumW {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int counter=0;
+        while(counter<100){
+            System.out.println("Hello World");
+            counter++;
+
+        }
+        System.out.println("Printed Hello world 100x");
+    }
+    
+}
