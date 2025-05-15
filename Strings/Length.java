cffd1847c93d0e80eb8a662302b67c2b21db553c
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-05-15 18:53:13 +0530

    Delete Strings directory

--- Strings/Length.java
+++
@@ -1,9 +0,0 @@
-package Strings;
-
-public class Length {
-    public static void main(String[] args) {
-        String FullName="Kashmira Nigholkar";
-        System.out.println(FullName.length());
-    }
-    
-}
