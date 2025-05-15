c354541c11a07f1f93d3549eb6f0d9a137cd498a
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-05-15 18:50:54 +0530

    Add files via upload

---
+++ Strings/Concatenate.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Concatenate {
+    public static void main(String[] args) {
+        String FirstName="Kashmira";
+        String LastName="Nigholkar";
+        String FullName=FirstName+" "+LastName;
+        System.out.println(FullName+" is this");
+
+}
+}
---
+++ Strings/Length.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class Length {
+    public static void main(String[] args) {
+        String FullName="Kashmira Nigholkar";
+        System.out.println(FullName.length());
+    }
+    
+}
