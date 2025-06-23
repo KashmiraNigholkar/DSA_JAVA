92fe32b205a4280de69f57ee62f2a6b21abc6780
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-06-23 18:19:36 +0530

    Add files via upload

---
+++ ArrayList/PairSum.java
@@ -0,0 +1,30 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class PairSum {
+    public static boolean PairSum(ArrayList<Integer>list,int target){
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                if(list.get(i)+list.get(j)==target){
+                    return true;
+
+                }
+
+            }
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target=60;
+        System.out.println(PairSum(list,target));
+    }
+    
+}
