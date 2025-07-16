b42024aa343bbb7ae9475b21707d21aa18353a8c
Kashmira Nigholkar <kashmiranigholkar421@gmail.com>
2025-07-16 22:16:08 +0530

    Add files via upload

---
+++ BinaryTrees/TreePreOrder.java
@@ -0,0 +1,43 @@
+package BinaryTrees;
+
+public class TreePreOrder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTrees {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, -1};
+        Node root = BinaryTrees.buildTree(nodes);
+
+        if (root != null) {
+            System.out.println("Root node data: " + root.data);
+        } else {
+            System.out.println("Tree is empty");
+        }
+    }
+}
